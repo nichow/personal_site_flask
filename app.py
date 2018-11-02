52f4bbe0d26c123b04d8fcab06469a442dedf659
nichow <nhgarcia27@gmail.com>
2018-11-01 23:58:01 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/aboutsite')
+def aboutsite():
+    return render_template('aboutsite.html')
+
+
+@app.route('/contact.html')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/experience')
+def experience():
+    return render_template('experience.html')
+
+
+@app.route('/music')
+def music():
+    return render_template('music.html')
+
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+
+@app.route('/writing')
+def writing():
+    return render_template('writing.html')
+
+
+if __name__ == '__main__':
+    app.run()
